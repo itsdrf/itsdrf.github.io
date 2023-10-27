1d2b5a454632b4c2c932705f651d6f8f79543e1a
itsdrf <debora.ramos005@gmail.com>
2023-10-26 23:48:46 -0300

    quarta versao

--- crud.js
+++ crud.js
@@ -1,3 +1,11 @@
+const openModal = () => document.getElementById('modal')
+    .classList.add('active')
+
+const closeModal = () => {
+    clearinfo()
+    document.getElementById('modal').classList.remove('active')
+}
+
 const getLocalstorage = () => JSON.parse(localStorage.getItem('dbrec')) ?? [];
 const setLocalstorage = (dbrec) => localStorage.setItem("dbrec", JSON.stringify(dbrec))
 
@@ -31,4 +39,35 @@ const deleteReceita = (index) =>{
     dbrec.splice(index,1)
     setLocalstorage(dbrec)
 }
+const isValidFields = () => {
+    document.getElementById('form').reportValidity();
+}
 
+const clearinfo = () =>{
+    const info = document.querySelectorAll('.modal-info')
+    info.forEach(info => info.value = "" )
+}
+
+const saveReceita = () =>{
+    if (isValidFields()){
+        const receita = {
+            Nome: document.getElementById('Nome').value,
+            Ingredientes: document.getElementById('Ingredientes').value,
+            Preparo: document.getElementById('Preparo').value,
+            Tempo: document.getElementById('Tempo').value,
+        }
+        createReceita(receita)
+        clearinfo()
+        closeModal  ()
+        console.log ("Criando nova receita")
+    }
+}
+
+document.getElementById('cadastrarReceita')
+.addEventListener('click', openModal)
+
+document.getElementById('modalClose')
+.addEventListener('click', closeModal)
+
+document.getElementById('salvar')
+.addEventListener('click', saveReceita)
